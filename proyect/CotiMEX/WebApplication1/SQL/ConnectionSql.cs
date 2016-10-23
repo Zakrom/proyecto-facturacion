49bd0a16a603a04ef12267c3c4cea57f9c1c33e6
Zakrom <edgararturo@msn.com>
2016-10-23 13:48:17 -0500

    create test proyect and connection

---
+++ proyect/CotiMEX/WebApplication1/SQL/ConnectionSql.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.SQL
+{
+    public class ConnectionSql
+    {
+        private static string cs = @"server=localhost;Uid=root;persistsecurityinfo=True;pwd=password123;database=cotimex";
+
+        //TODO load from properties connection string
+        public ConnectionSql()
+        {
+
+        }
+
+        public static void Main()
+        {
+
+            MySqlConnection conn = null;
+
+            try
+            {
+                conn = new MySqlConnection(cs);
+                conn.Open();
+                Console.WriteLine("Connection initialized");
+                Console.WriteLine("MySQL version : {0}", conn.ServerVersion);
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.ToString());
+
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                    Console.WriteLine("Connection closed");
+                }
+            }
+        }
+    }
+}
